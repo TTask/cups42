c52a40c1119656a6151cd2f488fb5afc25de1623
floyd <andrey.storchak@gmail.com>
2013-11-14 14:18:53 +0200

    marge confilcts

--- cups42/cups42/wsgi.py
+++ cups42/cups42/wsgi.py
@@ -1,9 +1,6 @@
 """
-<<<<<<< HEAD
 WSGI config for cups42 project.
 =======
-WSGI config for adminfor project.
->>>>>>> t1_show_user_info
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,11 +9,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-<<<<<<< HEAD
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cups42.settings")
 =======
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adminfor.settings")
->>>>>>> t1_show_user_info
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
