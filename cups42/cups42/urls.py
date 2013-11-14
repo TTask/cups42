4c5d2a910bf009479cd737a16d0301bd7dd05cff
floyd <andrey.storchak@gmail.com>
2013-11-14 13:04:16 +0200

    t:1 create the basic django project tha would present your name, surname, date of birth, contacts on the main page

---
+++ cups42/cups42/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'pyta.views.home_view', name='home'),
+    url(r'^index$', 'pyta.views.home_view', name='home'),
+    url(r'^edit$', 'pyta.views.edit_view', name='edit'),
+    url(r'^login$', 'pyta.views.login_view', name='login'),
+    url(r'^logout$', 'pyta.views.logout_view', name='logout'),
+    url(r'^admin/', include(admin.site.urls)),
+)
