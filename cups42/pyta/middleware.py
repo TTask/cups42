d45234b0f370105502d8c3e5d9da637d66be4591
Floyd <andrey.storchak@gmail.com>
2013-11-16 14:07:16 +0200

    rebuild repository

--- cups42/pyta/middleware.py
+++
@@ -1,7 +0,0 @@
-from models import RequestHistoryEntry
-
-
-class RequestStorage():
-    def process_request(self, request):
-        RequestHistoryEntry.objects.create(request_path=request.path, 
-            request_method=request.method)
