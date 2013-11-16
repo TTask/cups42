66625c133003375607db9e7f47491cd1cc2f47ec
Floyd <andrey.storchak@gmail.com>
2013-11-16 11:25:56 +0200

    getbarista trying

--- cups42/wsgi.py
+++ cups42/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cups42.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
