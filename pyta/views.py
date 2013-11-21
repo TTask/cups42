9a86e8cd200c07ead495254c669cab9a3ce443e4
Floyd <andrey.storchak@gmail.com>
2013-11-21 15:36:26 +0200

    Excluded elements that dont need to be used

--- pyta/views.py
+++ pyta/views.py
@@ -37,52 +37,47 @@ def edit_view(request):
     '''Edit user info view, allows user to edit data
     represented on the main page'''
     user_info = UserInfo.objects.get_or_create(pk=1)[0]
-    if request.method == 'POST':
-        edit_form = EditUserInfoForm(request.POST,
-                                     request.FILES,
-                                     instance=user_info)
-        if edit_form.is_valid():
-            edit_form.save()
-            return redirect(reverse('home'))
-        else:
-            return render_to_response(
-                'edit.html',
-                {'edit_form': edit_form},
-                context_instance=RequestContext(request))
-    else:
+    if request.method=='GET':
         edit_form = EditUserInfoForm(instance=user_info)
+        return render_to_response('edit.html', {'edit_form': edit_form},
+                                  context_instance=RequestContext(request))
+    edit_form = EditUserInfoForm(request.POST or None,
+                                request.FILES,
+                                instance=user_info)
+    if edit_form.is_valid():
+        edit_form.save()
+        return redirect(reverse('home'))
+    else:
         return render_to_response(
             'edit.html',
             {'edit_form': edit_form},
             context_instance=RequestContext(request))
 
 
+
 @login_required(login_url='/login')
 def edit_view_ajax(request):
     '''This view the same as edit view, but used only if edit form
     submited with ajax'''
+    response = {}
     user_info = UserInfo.objects.get_or_create(pk=1)[0]
-    if request.method == 'POST' and request.is_ajax():
-        response = {}
-        edit_form = EditUserInfoForm(
-            request.POST,
-            request.FILES,
-            instance=user_info)
-        if edit_form.is_valid():
-            edit_form.save()
-            response['request_result'] = 'Successfuly saved.'
-            return HttpResponse(
-                json.dumps(response),
-                content_type='application/json')
-        else:
-            response = dict(
-                [field, error] for field, error in edit_form.errors.items())
-            response['request_result'] = 'Error occurred'
-            return HttpResponse(
-                json.dumps(response),
-                content_type='application/json')
+    edit_form = EditUserInfoForm(
+        request.POST or None,
+        request.FILES,
+        instance=user_info)
+    if edit_form.is_valid():
+        edit_form.save()
+        response['request_result'] = 'Successfuly saved.'
+        return HttpResponse(
+            json.dumps(response),
+            content_type='application/json')
     else:
-        return edit_view(request)
+        response = dict(
+            [field, error] for field, error in edit_form.errors.items())
+        response['request_result'] = 'Error occurred'
+        return HttpResponse(
+            json.dumps(response),
+            content_type='application/json')
 
 
 @login_required(login_url='/login')
@@ -91,16 +86,17 @@ def set_request_priority_view_ajax(request):
     response = {}
     form = EditRequestPriorityForm(request.POST or None)
     if form.is_valid():
+        req_priority = form.cleaned_data['request_priority']
+        req_path = form.cleaned_data['request_path']
         try:
             request_history = RequestHistoryEntry.objects.filter(
-                request_path=form.cleaned_data['request_path']).all()
-            request_history.update(
-                request_priority=form.cleaned_data['request_priority'])
+                request_path=req_path).all()
+            request_history.update(request_priority=req_priority)
         except ObjectDoesNotExist:
-            request_history = []
+            pass
         priority_entry = RequestPriorityEntry.objects.get_or_create(
-            request_path=form.cleaned_data['request_path'])[0]
-        priority_entry.request_priority = form.cleaned_data['request_priority']
+            request_path=req_path)[0]
+        priority_entry.request_priority = req_priority
         priority_entry.save()
         response['request_result'] = 'Priority changed successfull'
         return HttpResponse(json.dumps(response),
