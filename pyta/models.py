238782647ca8395dcdc3977888c4dbdf161e0ad1
Floyd <andrey.storchak@gmail.com>
2013-11-21 09:25:31 +0200

    Move signal-processors to models

--- pyta/models.py
+++ pyta/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from sorl.thumbnail import ImageField
+from django.db.models.signals import post_save
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 
 class UserInfo(models.Model):
@@ -71,3 +74,34 @@ class RequestPriorityEntry(models.Model):
                                   verbose_name="Request path")
   request_priority = models.IntegerField(default=0,
                                         verbose_name="Priority")
+
+
+#-----------------------------------------------------------------------------#
+#                                SIGNAL PROCESSORS                            #
+#-----------------------------------------------------------------------------#
+@receiver(post_save)
+def save_handler(sender, **kwargs):
+    if not sender.__name__ == ModelHistoryEntry.__name__:
+        try:
+            if kwargs['created']:
+                ModelHistoryEntry.objects.create(
+                    model_name=sender.__name__,
+                    change_type="CREATED")
+
+            else:
+                ModelHistoryEntry.objects.create(
+                    model_name=sender.__name__,
+                    change_type="MODIFIED")
+        except:
+            return
+
+
+@receiver(post_delete)
+def delete_handler(sender, **kwargs):
+    try:
+        ModelHistoryEntry.objects.create(
+            model_name=sender.__name__,
+            change_type="DELETED")
+    except:
+        return
+
